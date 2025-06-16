c48caa38f1e30bc521ed0dcd9c726317fedd4c05
Marcin Wosinek <marcin.wosinek@gmail.com>
2025-06-16 20:08:34 +0200

    show in console.log visible links

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
-console.log("Link Analyzer Plugin: Hello World!");
+// TODO decide on the trade-off: as we wait for images to load, the user may be
+// scrolling down.
+window.addEventListener("load", function (event) {
+	console.log("Link Analyzer Plugin: Hello World!");
+
+	const screenWidth = window.innerWidth,
+		screenHeight = window.innerHeight;
+
+	console.log(`${screenWidth}, ${screenHeight}`);
+
+	// all links that are not WP admin bar
+	const links = document.querySelectorAll(
+		"body > div:not(#wpadminbar) a[href]",
+	);
+
+	// consider using lodash
+	const aboveFoldLinks = Array.from(links).filter((element) => {
+		const { y } = element.getBoundingClientRect();
+
+		return y < screenHeight;
+	});
+
+	console.log(aboveFoldLinks);
+});
